4d8ff591a0f7d0406e8fb7bc6c04b20d5d97b555
glstream <grayson.stream@gmail.com>
2018-12-23 13:16:38 -0800

    win changes

--- weather2.py
+++ weather2.py
@@ -1,5 +1,5 @@
 import forecastio
-
+from configs import *
 
 lat = "47.57070050000001"
 lng = "-122.38715259999998"
@@ -7,10 +7,10 @@ lng = "-122.38715259999998"
 
 forecast = forecastio.load_forecast(api_key, lat, lng)
 
-byHour = forecast.hourly()
-print(byHour.summary)
-print(byHour.icon)
+current = forecast.currently()
+print(current.summary)
+print(current.icon)
 
-for hourlyData in byHour.data:
-    print(hourlyData.temperature)
+# for hourlyData in byHour.data:
+#     print(hourlyData.temperature)
 
