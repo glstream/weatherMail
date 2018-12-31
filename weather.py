485a806afe9aef4af2eb7f4799852c4376c3de8b
glstream <grayson.stream@gmail.com>
2018-12-31 12:57:59 -0800

    added darksky

---
+++ weather.py
@@ -0,0 +1,18 @@
+from darksky import forecast
+from configs import *
+
+
+key = "43c6b72e8be91162c3aa43f4f87be22d"
+lat = "47.57070050000001"
+lon = "-122.38715259999998"
+
+
+forecast = forecast(api_key, lat, lon)
+
+icon = forecast.currently.icon
+temperature = forecast.currently.temperature
+
+
+
+print(icon)
+print(temperature)
